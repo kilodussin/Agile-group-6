77d8bd2a7ae6108a2d5d0644e7460ab33d5756dd
Chloe Dusén <kilodussin@gmail.com>
2025-05-03 16:15:47 +0200

    added a api implementation

---
+++ Tests/APITests.java
@@ -0,0 +1,10 @@
+package Tests;
+import Model.API.API;
+import org.junit.jupiter.api.Test;
+
+public class APITests {
+    @Test
+    void APITest(){
+        System.out.println(API.getFunFact(2));
+    }
+}
